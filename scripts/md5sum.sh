606e3696304b4ee2731fa8dd19a0a987b13386c4
Michael Bai <80080260+mike-bai@users.noreply.github.com>
2022-07-26 17:47:16 -0400

    Add files via upload

---
+++ scripts/md5sum.sh
@@ -0,0 +1,13 @@
+#Check integrity of fastq files following transfer
+
+
+# -c: read m5 sums from file and check them
+# --quiet: don't print ok messages, only error messages
+mkdir ../qc_seqs/md5sums
+for file in /workdir/myb7/data/MuskegonLake/qc_seqs/s916*/*/*.fastq
+do
+	md5sum $file > ${file}_checkmd5.md5
+	md5sum -c --quiet ${file}_checkmd5.md5
+	mv /workdir/myb7/data/MuskegonLake/scripts/${file}_checkmd5.md5 /workdir/myb7/data/MuskegonLake/qc_seqs/md5sums
+done
+echo "Finished testing files"
